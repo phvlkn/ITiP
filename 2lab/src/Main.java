12c099198194aa563851a9db47c9ad642b4fb3b9
phvlkn <eminislamow@googlemail.com>
2025-10-23 14:34:22 +0300

    laba2

--- 2lab/src/Main.java
+++ 2lab/src/Main.java
@@ -3,6 +3,9 @@ public class Main {
         Refrigerator fridge = new Refrigerator("LG", 250, 45000, 3.5);
         Dishwasher washer = new Dishwasher("Bosch", 1800, 60000, 12);
         VacuumCleaner vacuum = new VacuumCleaner("Dyson", 900, 70000, "Беспроводной");
+        fridge.setBrand("Liebherr");
+        Dishwasher appl = new Dishwasher();
+        System.out.println(appl.getBrand());
 
         fridge.turnOn();
         washer.turnOn();
